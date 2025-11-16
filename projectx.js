12d8b3c4dac8cb2bd3160ba6b76a87819d92d0c3
J S <smutek.jakub@seznam.cz>
2025-11-16 18:40:42 +0100

    Updates

--- projectx.js
+++ projectx.js
@@ -10944,14 +10944,14 @@ function checkIncomingModuleAPI() {
 }
 
 var ASM_CONSTS = {
-  2164677: () => {
-    var buf = Module.soundBuffers["WordCorrect1"];
+  2164725: $0 => {
+    var buf = Module.soundBuffers[$0];
     var src = Module.sfxCtx.createBufferSource();
     src.buffer = buf;
     src.connect(Module.sfxCtx.destination);
     src.start();
   },
-  2164845: $0 => {
+  2164881: $0 => {
     var str = UTF8ToString($0) + "\n\n" + "Abort/Retry/Ignore/AlwaysIgnore? [ariA] :";
     var reply = window.prompt(str, "i");
     if (reply === null) {
@@ -10959,7 +10959,7 @@ var ASM_CONSTS = {
     }
     return allocate(intArrayFromString(reply), "i8", ALLOC_NORMAL);
   },
-  2165070: () => {
+  2165106: () => {
     if (typeof (AudioContext) !== "undefined") {
       return true;
     } else if (typeof (webkitAudioContext) !== "undefined") {
@@ -10967,7 +10967,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  2165217: () => {
+  2165253: () => {
     if ((typeof (navigator.mediaDevices) !== "undefined") && (typeof (navigator.mediaDevices.getUserMedia) !== "undefined")) {
       return true;
     } else if (typeof (navigator.webkitGetUserMedia) !== "undefined") {
@@ -10975,7 +10975,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  2165451: $0 => {
+  2165487: $0 => {
     if (typeof (Module["SDL2"]) === "undefined") {
       Module["SDL2"] = {};
     }
@@ -10999,11 +10999,11 @@ var ASM_CONSTS = {
     }
     return SDL2.audioContext === undefined ? -1 : 0;
   },
-  2166003: () => {
+  2166039: () => {
     var SDL2 = Module["SDL2"];
     return SDL2.audioContext.sampleRate;
   },
-  2166071: ($0, $1, $2, $3) => {
+  2166107: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     var have_microphone = function(stream) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -11045,7 +11045,7 @@ var ASM_CONSTS = {
       }, have_microphone, no_microphone);
     }
   },
-  2167764: ($0, $1, $2, $3) => {
+  2167800: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     SDL2.audio.scriptProcessorNode = SDL2.audioContext["createScriptProcessor"]($1, 0, $0);
     SDL2.audio.scriptProcessorNode["onaudioprocess"] = function(e) {
@@ -11077,7 +11077,7 @@ var ASM_CONSTS = {
       SDL2.audio.silenceTimer = setInterval(silence_callback, ($1 / SDL2.audioContext.sampleRate) * 1e3);
     }
   },
-  2168939: ($0, $1) => {
+  2168975: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.capture.currentCaptureBuffer.numberOfChannels;
     for (var c = 0; c < numChannels; ++c) {
@@ -11096,7 +11096,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  2169544: ($0, $1) => {
+  2169580: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var buf = $0 >>> 2;
     var numChannels = SDL2.audio.currentOutputBuffer["numberOfChannels"];
@@ -11110,7 +11110,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  2170033: $0 => {
+  2170069: $0 => {
     var SDL2 = Module["SDL2"];
     if ($0) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -11144,7 +11144,7 @@ var ASM_CONSTS = {
       SDL2.audioContext = undefined;
     }
   },
-  2171039: ($0, $1, $2) => {
+  2171075: ($0, $1, $2) => {
     var w = $0;
     var h = $1;
     var pixels = $2;
@@ -11215,7 +11215,7 @@ var ASM_CONSTS = {
     }
     SDL2.ctx.putImageData(SDL2.image, 0, 0);
   },
-  2172507: ($0, $1, $2, $3, $4) => {
+  2172543: ($0, $1, $2, $3, $4) => {
     var w = $0;
     var h = $1;
     var hot_x = $2;
@@ -11252,18 +11252,18 @@ var ASM_CONSTS = {
     stringToUTF8(url, urlBuf, url.length + 1);
     return urlBuf;
   },
-  2173495: $0 => {
+  2173531: $0 => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = UTF8ToString($0);
     }
   },
-  2173578: () => {
+  2173614: () => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = "none";
     }
   },
-  2173647: () => window.innerWidth,
-  2173677: () => window.innerHeight
+  2173683: () => window.innerWidth,
+  2173713: () => window.innerHeight
 };
 
 function setAssetsLoaded() {
