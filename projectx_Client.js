1632897cbe75bc1ea68ab8dca00be229c525d425
J S <smutek.jakub@seznam.cz>
2026-02-08 11:08:55 +0100

    Updates

--- projectx_Client.js
+++ projectx_Client.js
@@ -11060,7 +11060,7 @@ function checkIncomingModuleAPI() {
 }
 
 var ASM_CONSTS = {
-  1063194: $0 => {
+  1061914: $0 => {
     var str = UTF8ToString($0) + "\n\n" + "Abort/Retry/Ignore/AlwaysIgnore? [ariA] :";
     var reply = window.prompt(str, "i");
     if (reply === null) {
@@ -11068,7 +11068,7 @@ var ASM_CONSTS = {
     }
     return allocate(intArrayFromString(reply), "i8", ALLOC_NORMAL);
   },
-  1063419: () => {
+  1062139: () => {
     if (typeof (AudioContext) !== "undefined") {
       return true;
     } else if (typeof (webkitAudioContext) !== "undefined") {
@@ -11076,7 +11076,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  1063566: () => {
+  1062286: () => {
     if ((typeof (navigator.mediaDevices) !== "undefined") && (typeof (navigator.mediaDevices.getUserMedia) !== "undefined")) {
       return true;
     } else if (typeof (navigator.webkitGetUserMedia) !== "undefined") {
@@ -11084,7 +11084,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  1063800: $0 => {
+  1062520: $0 => {
     if (typeof (Module["SDL2"]) === "undefined") {
       Module["SDL2"] = {};
     }
@@ -11108,11 +11108,11 @@ var ASM_CONSTS = {
     }
     return SDL2.audioContext === undefined ? -1 : 0;
   },
-  1064352: () => {
+  1063072: () => {
     var SDL2 = Module["SDL2"];
     return SDL2.audioContext.sampleRate;
   },
-  1064420: ($0, $1, $2, $3) => {
+  1063140: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     var have_microphone = function(stream) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -11154,7 +11154,7 @@ var ASM_CONSTS = {
       }, have_microphone, no_microphone);
     }
   },
-  1066113: ($0, $1, $2, $3) => {
+  1064833: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     SDL2.audio.scriptProcessorNode = SDL2.audioContext["createScriptProcessor"]($1, 0, $0);
     SDL2.audio.scriptProcessorNode["onaudioprocess"] = function(e) {
@@ -11186,7 +11186,7 @@ var ASM_CONSTS = {
       SDL2.audio.silenceTimer = setInterval(silence_callback, ($1 / SDL2.audioContext.sampleRate) * 1e3);
     }
   },
-  1067288: ($0, $1) => {
+  1066008: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.capture.currentCaptureBuffer.numberOfChannels;
     for (var c = 0; c < numChannels; ++c) {
@@ -11205,7 +11205,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  1067893: ($0, $1) => {
+  1066613: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var buf = $0 >>> 2;
     var numChannels = SDL2.audio.currentOutputBuffer["numberOfChannels"];
@@ -11219,7 +11219,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  1068382: $0 => {
+  1067102: $0 => {
     var SDL2 = Module["SDL2"];
     if ($0) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -11253,7 +11253,7 @@ var ASM_CONSTS = {
       SDL2.audioContext = undefined;
     }
   },
-  1069388: ($0, $1, $2) => {
+  1068108: ($0, $1, $2) => {
     var w = $0;
     var h = $1;
     var pixels = $2;
@@ -11324,7 +11324,7 @@ var ASM_CONSTS = {
     }
     SDL2.ctx.putImageData(SDL2.image, 0, 0);
   },
-  1070856: ($0, $1, $2, $3, $4) => {
+  1069576: ($0, $1, $2, $3, $4) => {
     var w = $0;
     var h = $1;
     var hot_x = $2;
@@ -11361,18 +11361,18 @@ var ASM_CONSTS = {
     stringToUTF8(url, urlBuf, url.length + 1);
     return urlBuf;
   },
-  1071844: $0 => {
+  1070564: $0 => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = UTF8ToString($0);
     }
   },
-  1071927: () => {
+  1070647: () => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = "none";
     }
   },
-  1071996: () => window.innerWidth,
-  1072026: () => window.innerHeight
+  1070716: () => window.innerWidth,
+  1070746: () => window.innerHeight
 };
 
 function setAssetsLoaded() {
@@ -11931,7 +11931,6 @@ var wasmImports = {
   /** @export */ glGenTextures: _glGenTextures,
   /** @export */ glGenVertexArrays: _glGenVertexArrays,
   /** @export */ glGenerateMipmap: _glGenerateMipmap,
-  /** @export */ glGetIntegerv: _glGetIntegerv,
   /** @export */ glGetProgramInfoLog: _glGetProgramInfoLog,
   /** @export */ glGetProgramiv: _glGetProgramiv,
   /** @export */ glGetShaderInfoLog: _glGetShaderInfoLog,
@@ -11943,6 +11942,7 @@ var wasmImports = {
   /** @export */ glShaderSource: _glShaderSource,
   /** @export */ glTexImage2D: _glTexImage2D,
   /** @export */ glTexParameteri: _glTexParameteri,
+  /** @export */ glTexSubImage2D: _glTexSubImage2D,
   /** @export */ glUniform1f: _glUniform1f,
   /** @export */ glUniform1i: _glUniform1i,
   /** @export */ glUniform2fv: _glUniform2fv,
